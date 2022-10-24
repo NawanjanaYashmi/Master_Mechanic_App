eda46e661b17d81e7ac4a5e5e56620158f7fb9c2
IT21061134 <nawanjanayeshmi@gmail.com>
2022-10-24 10:53:01 +0530

    update

--- app/src/main/java/com/example/master_mechanic_app/M_Mechanic.java
+++ app/src/main/java/com/example/master_mechanic_app/M_Mechanic.java
@@ -2,13 +2,39 @@ package com.example.master_mechanic_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class M_Mechanic extends AppCompatActivity {
+
+    Button loginBtn;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mmechanic);
+
+        loginBtn = findViewById(R.id.login_btn);
+
+
+       loginBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(getApplicationContext(),WelcomePage.class));
+           }
+       });
+
+
+
     }
+
+
+
+
 }
